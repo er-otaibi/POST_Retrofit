15947182be65f988f1e172739f0778ad79dd43b6
Rehab B <79226830+er-otaibi@users.noreply.github.com>
2021-10-10 19:59:39 +0300

    post

---
+++ POSTRetrofit/app/src/main/java/com/example/postretrofit/UserAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.postretrofit
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.user_row.view.*
+
+class UserAdapter( private val myUsers: List<Users.UserDetails>):  RecyclerView.Adapter<UserAdapter.ItemViewHolder>(){
+    class ItemViewHolder (itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.user_row,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val list1 =myUsers[position]
+
+        holder.itemView.apply {
+            tvname.text = list1.name
+            tvlocation.text = list1.location
+
+
+        }
+    }
+
+    override fun getItemCount() = myUsers.size
+}
